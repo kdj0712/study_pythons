6386a6928e29f4173ff2b11f2bc18e4b0c05aebc
DeokJae Kim <kdj0712@gmail.com>
2023-12-04 12:49:18 +0900

    first commit

---
+++ docs/welcomes.py
@@ -0,0 +1,14 @@
+# 변수 선언  후 정의할 떄 고려할 부분(넣는 값 = 문자 or 숫자)
+# 문자 출력
+helloworld="hello, world, Deok Jae Kim." #(문자형 변수)
+print(helloworld)
+
+# 숫자 합산 출력
+numbers = 3 + 5     #(숫자형 변수)
+print(numbers)
+
+# 데이터 타입 : 문장형 or 숫자형 등 변수에 대한 정의
+
+# if 구문 사용
+if 5 > 2:
+  print("Five is greater than two!")
