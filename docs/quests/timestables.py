ddc705e092529eeca832889e37798032ec542ba1
DeokJae Kim <kdj0712@gmail.com>
2023-12-06 09:15:44 +0900

    주현님 구문 참조 버전

--- docs/quests/timestables.py
+++ docs/quests/timestables.py
@@ -1,11 +1,11 @@
-main = 0
-while main < 9:
-    pass
-    main = main + 1
-    answer = 5 * main 
-    print("{} = 5 * {}".format(answer,main))
-    pass
-print("End Program!")
+# main = 0
+# while main < 9:
+#     pass
+#     main = main + 1
+#     answer = 5 * main 
+#     print("{} = 5 * {}".format(answer,main))
+#     pass
+# print("End Program!")
 # question = "몇 단을 보실건가요? : "
 # num_want = input("{}".format(question)).split()
 # want = int(num_want)
@@ -17,3 +17,14 @@ print("End Program!")
 #     print("{} = {} * {}".format(answer,want,main))
 #     pass
 # print("End Program!")
+
+want = int(input("몇 단을 보실 건가요? : "))
+main = 0
+while main < 9:
+    pass
+    main = main + 1
+
+    print("{} * {} = {}".format(want,main,want * main))
+    if main > 9:
+        pass
+    pass
