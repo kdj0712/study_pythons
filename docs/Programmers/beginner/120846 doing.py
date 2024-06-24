ca77663820d89c9cf16660776b11cb9381364077
DeokJae Kim <151099214+kdj0712@users.noreply.github.com>
2024-06-24 17:34:41 +0900

    commit

--- docs/Programmers/beginner/120846 doing.py
+++ docs/Programmers/beginner/120846 doing.py
@@ -4,15 +4,35 @@
 
 # 약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
 
+# def solution(n):
+#     answer = 0
+#     for x in range(n):
+#         for y in range(x+1):
+#             (x+1)%y == 0
+#     return answer
+
+# def solve():
+#     n = 10
+#     answer = solution(n)
+#     print("{}".format(answer))
+#     return
+
+# solve()
+
+
 def solution(n):
     answer = 0
-    for x in range(n):
-        for y in range(x+1):
-            (x+1)%y == 0
+    for i in range(2, n+1):
+        cnt = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                cnt += 1
+        if cnt >= 3:
+            answer += 1
     return answer
 
 def solve():
-    n = 10
+    n = 15
     answer = solution(n)
     print("{}".format(answer))
     return
